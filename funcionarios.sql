d9562a53286bb1b4d3691d1474b70ccce35b203a
Sérgio Leite <eng.sergio.leite@gmail.com>
2026-03-16 15:25:46 -0300

    Estrutura inicial e consultas básicas (1 - 5)

--- funcionarios.sql
+++ funcionarios.sql
@@ -1,3 +1,5 @@
+-- Questão 1 e 2: Criação e visualização inicial
+
 CREATE TABLE funcionarios(
     id INT PRIMARY KEY, 
     nome VARCHAR(100),
@@ -5,22 +7,27 @@ CREATE TABLE funcionarios(
     salario FLOAT
 );
 
--- SELECT * FROM funcionarios;
-
 INSERT INTO funcionarios
 (id, nome, departamento, salario)
 VALUES
-(
- 1,  'Heitor Vieira',  'Financeiro',  4959.22
- 2,  'Daniel Campos',  'Vendas',  3884.44
- 3,  'Luiza Dias',  'TI',  8205.78
- 4,  'Davi Lucas Moraes',  'Financeiro',  8437.02
- 5,  'Pietro Cavalcanti',  'TI',  4946.88
- 6,  'Evelyn da Mata',  'Vendas',  5278.88
- 7,  'Isabella Rocha',  'Marketing',  4006.03
- 8,  'Sra. Manuela Azevedo',  'Vendas',  6101.88
- 9,  'Brenda Cardoso',  'TI',  8853.34
- 10, 'Danilo Souza',  'TI',  8242.14
-);
+(1,  'Heitor Vieira',  'Financeiro',  4959.22),
+(2,  'Daniel Campos',  'Vendas',  3884.44),
+(3,  'Luiza Dias',  'TI',  8205.78),
+(4,  'Davi Lucas Moraes',  'Financeiro',  8437.02),
+(5,  'Pietro Cavalcanti',  'TI',  4946.88),
+(6,  'Evelyn da Mata',  'Vendas',  5278.88),
+(7,  'Isabella Rocha',  'Marketing',  4006.03),
+(8,  'Sra. Manuela Azevedo',  'Vendas',  6101.88),
+(9,  'Brenda Cardoso',  'TI',  8853.34),
+(10, 'Danilo Souza',  'TI',  8242.14);
 
+SELECT * FROM funcionarios;
+
+-- Questão 3 e 4 e: Consultas com filtro
+
+SELECT nome FROM funcionarios WHERE departamento = 'Vendas'; 
+
+SELECT * FROM funcionarios where salario > 5000;
+
+SELECT DISTINCT departamento FROM funcionarios; 
 
